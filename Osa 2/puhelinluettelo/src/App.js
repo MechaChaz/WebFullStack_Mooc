d646fd0587b4e920ea35ad90dfa8b1a662ace5ec
MechaChaz <kortelainen.jaakko@gmail.com>
2021-01-04 06:43:08 +0200

    Set upping new comp

--- Osa 2/puhelinluettelo/src/App.js
+++ Osa 2/puhelinluettelo/src/App.js
@@ -6,7 +6,8 @@ const App = () => {
     { name: 'Arto Hellas', number: '040-123456', id: 1 },
     { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
     { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
+    { name: 'Matti Meikäläinen', number: '0400-123-456', id: 5}
   ]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber] = useState('')
